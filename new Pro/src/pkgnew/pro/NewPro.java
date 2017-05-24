363218f62203523d6ac6f31459c3a8abe37d442f
Michale <Michale@Memey-PC>
2017-05-23 21:45:55 -0700

    our 1st program

---
+++ new Pro/src/pkgnew/pro/NewPro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgnew.pro;
+
+/**
+ *
+ * @author Michale
+ */
+public class NewPro {
+
+    
+    public static void main(String[] args) {
+    System.out.println("Hello world");
+    }
+    
+}
